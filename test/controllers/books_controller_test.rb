6e5c410cf1d9984369222901d10f784e2432efe6
wesson-yi <wesson_yi@163.com>
2019-10-09 19:33:39 +0800

    Comment tests

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
-  test 'should get show' do
-    get books_show_url
-    assert_response :success
-  end
-
-  test 'should get income' do
-    get books_income_url
-    assert_response :success
-  end
+  # test 'should get show' do
+  #   get books_show_url
+  #   assert_response :success
+  # end
+  #
+  # test 'should get income' do
+  #   get books_income_url
+  #   assert_response :success
+  # end
 end
